971720af3ecc8f7f5d4c879b53d6ca852f4d516b
Devin Curry - Laptop <Devination3D@gmail.com>
2018-01-27 01:16:10 -0800

    Death animation wip

---
+++ Global Game Jam 2018/Assets/Scripts/NPCs/GeneralNPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class GeneralNPC : MonoBehaviour {
+	Animator animator;
+	[SerializeField] GameObject deathFX;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	void PlayDeathFX () {
+		Instantiate(deathFX, transform.position, Quaternion.identity);
+	}
+
+	void Die () {
+		animator.SetTrigger("Die");
+		Destroy(gameObject, 10);
+	}
+}
