4fca863ae84a5474c4ccd860c19e9823ad96a645
Cody Malach <codymalach@gmail.com>
2018-01-28 13:35:36 -0800

    Push to start text

--- Global Game Jam 2018/Assets/Scripts/UI/TitleScene.cs
+++ Global Game Jam 2018/Assets/Scripts/UI/TitleScene.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class TitleScene : MonoBehaviour {
 	GameObject[] texts;
+	float timer = 0;
 
 	void Start () {
 		texts = new GameObject[] {
 			transform.Find( "Doctor" ).gameObject,
 			transform.Find( "Despicable" ).gameObject,
 			transform.Find( "Mansion" ).gameObject,
-			transform.Find( "Maladies" ).gameObject
+			transform.Find( "Maladies" ).gameObject,
+			transform.Find( "Start" ).gameObject
 		};
+	}
+
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 8 && Input.anyKeyDown) {
+			Application.LoadLevel( 1 );
+		}
 	}
 
 	//This is triggered from events within the TitleScreen animation
