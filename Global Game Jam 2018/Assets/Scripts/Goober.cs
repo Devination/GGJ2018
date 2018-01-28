dfd238f9583d03991f4b35696e34b7b20ee2704c
Cody Malach <codymalach@gmail.com>
2018-01-27 17:05:22 -0800

    Add goobsplosion

--- Global Game Jam 2018/Assets/Scripts/Goober.cs
+++ Global Game Jam 2018/Assets/Scripts/Goober.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent( typeof( Animator ) )]
 public class Goober : MonoBehaviour {
 	private static float SPEED = 8;
 	private static float DAMP_SCALAR = 0.3f;
 	private Rigidbody2D body;
+	private Animator animator;
 	public Vector2 direction;
+	[SerializeField] GameObject splatFX;
 
 	private void Awake () {
+		animator = GetComponent<Animator>();
 		body = GetComponent<Rigidbody2D>();
 	}
 
@@ -32,9 +36,14 @@ public class Goober : MonoBehaviour {
 		}
 	}
 
-	void Splatter() {
-		//TODO: gross animation
+	//animator "Splatter" trigger will trigger this.
+	public void PlaySplatFX () {
+		Instantiate( splatFX, transform.position, Quaternion.identity );
 		Destroy( gameObject );
+	}
+
+	void Splatter () {
+		animator.SetTrigger( "Splattered" );
 	}
 
 	void OnBecameInvisible () {
