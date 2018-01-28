20389e01e0d7397c618785b09f48ab024fd0437a
Devin Curry - Laptop <Devination3D@gmail.com>
2018-01-27 19:36:17 -0800

    Fix multiple member error

--- Global Game Jam 2018/Assets/Scripts/NPCs/FollowNPC.cs
+++ Global Game Jam 2018/Assets/Scripts/NPCs/FollowNPC.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class FollowNPC : GeneralNPC {
 	[SerializeField] float speed = 2;
-	Transform player;
-
-	protected override void Start () {
-		base.Start();
-		player = FindObjectOfType<Player>().transform;
-	}
 
 	void FixedUpdate () {
-		Vector2 dirToPlayer = player.position - transform.position;
+		Vector2 dirToPlayer = player.transform.position - transform.position;
 		body.velocity = dirToPlayer.normalized * speed;
 	}
 }
