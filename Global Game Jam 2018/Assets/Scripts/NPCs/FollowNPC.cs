752ccf446f2c56035068127ab6f665271bff3eaf
Devin Curry - Laptop <Devination3D@gmail.com>
2018-01-27 18:23:41 -0800

    FollowNPC

---
+++ Global Game Jam 2018/Assets/Scripts/NPCs/FollowNPC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowNPC : GeneralNPC {
+	[SerializeField] float speed = 2;
+	Transform player;
+
+	protected override void Start () {
+		base.Start();
+		player = FindObjectOfType<Player>().transform;
+	}
+
+	void FixedUpdate () {
+		Vector2 dirToPlayer = player.position - transform.position;
+		body.velocity = dirToPlayer.normalized * speed;
+	}
+}
