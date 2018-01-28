4d978ee91e216033caf0695004850f762e997cef
Cody Malach <codymalach@gmail.com>
2018-01-28 00:27:38 -0800

    Spawn logic

---
+++ Global Game Jam 2018/Assets/Scripts/NPCs/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	[SerializeField] GameObject[] npcPrefabs; 
+	[SerializeField] Vector2 range;
+
+	void Start () {
+		StartCoroutine( SpawnLoop() );
+	}
+	
+	IEnumerator SpawnLoop() {
+		while ( true ) {
+			yield return new WaitForSeconds( 5 );
+			Spawn();
+		}
+	}
+
+	void Spawn() {
+		Vector2 position = new Vector2( Random.Range( -range.x, range.x ), Random.Range( -range.y, range.y ) );
+		GameObject npc = npcPrefabs[Random.Range(0, npcPrefabs.Length)];
+		Instantiate( npc, position, Quaternion.identity );
+	}
+}
