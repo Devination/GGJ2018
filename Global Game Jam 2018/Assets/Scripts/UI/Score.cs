e7d24be85c3afe80e5dce4f093306226b2df010e
Cody Malach <codymalach@gmail.com>
2018-01-28 01:55:45 -0800

    * Setup snot sprite * Make empathy bar less bad. * Add score

---
+++ Global Game Jam 2018/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	float SCORE_MULTIPLIER = 50;
+	public Player player;
+	float score;
+	Text scoreText; 
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		scoreText = GameObject.Find( "Score" ).GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( player != null ) {
+			score += Time.deltaTime * SCORE_MULTIPLIER;
+			scoreText.text = string.Format( "{0:N0}", score );
+		}
+	}
+}
